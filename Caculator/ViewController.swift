6e98e5e3b0932f01ff9b991e1491228a74911bb4
Luis Esquivel <laesquivel@hotmail.com>
2017-01-23 12:35:15 -0500

    first commit; basic operations ready

--- Caculator/ViewController.swift
+++ Caculator/ViewController.swift
@@ -9,16 +9,74 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var previousValue = String()
+    var currentOperator = String()
+    var flagNewOperation = false
 
+    @IBOutlet weak var displayLabel: UILabel!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func numberPressed(_ sender: UIButton) {
+        if (displayLabel.text == "0" || flagNewOperation)
+        {
+            displayLabel.text = sender.currentTitle
+            flagNewOperation = false
+        } else {
+            displayLabel.text = "\(displayLabel.text!)\(sender.currentTitle!)"
+        }
+    }
+    
+    @IBAction func dotPressed(_ sender: UIButton) {
+        if !(displayLabel.text?.contains("."))! {
+            displayLabel.text = "\(displayLabel.text!)\(sender.currentTitle!)"
+        }
+    }
+    
+
+    @IBAction func operatorPressed(_ sender: UIButton) {
+        
+        flagNewOperation = true
+
+        if currentOperator != "" {
+    
+            switch currentOperator {
+            case "+":
+                displayLabel.text = "\(Float(previousValue)! + Float(displayLabel.text!)!)"
+            case "-":
+                displayLabel.text = "\(Float(previousValue)! - Float(displayLabel.text!)!)"
+            case "X":
+                displayLabel.text = "\(Float(previousValue)! * Float(displayLabel.text!)!)"
+            case "/":
+                displayLabel.text = "\(Float(previousValue)! / Float(displayLabel.text!)!)"
+            default:
+                displayLabel.text = displayLabel.text
+            }
+            
+        }
+
+        currentOperator = sender.currentTitle!
+        previousValue = displayLabel.text!
+    
+    }
+    
+    // Clear display
+    @IBAction func clearButton(_ sender: UIButton) {
+        displayLabel.text = "0"
+        previousValue = ""
+        currentOperator = ""
+    }
+    
 
 
 }
